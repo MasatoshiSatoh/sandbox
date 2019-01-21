aa2a48c472d5d145c8352b2a91b7ad8632ea7377
Masatoshi Satoh <s.masatoshi@gmail.com>
2019-01-21 20:46:26 +0900

    phpstanテスト

--- src/Sample.php
+++ src/Sample.php
@@ -4,12 +4,17 @@ namespace Sample;
  
 class Sample {
     // $a + $b を返す
-    public function Add($a, $b) {
+    public function Add(int $a, int $b): int {
         return $a + $b;
     }
  
     // $a - $b を返す
-    public function Sub($a, $b) {
+    public function Sub(int $a, int $b): int {
         return $a + $b;
+    }
+
+    // $a / $b を返す
+    public function div(int $a, int $b): int {
+        return $a / $c;
     }
 }
